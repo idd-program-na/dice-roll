b969d5a5ea40d9cf9c1d7b6ef9cb68bdba5b47d6
Nicole Aranzanso <naranzanso@Nicoles-MacBook-Pro.local>
2020-01-16 11:07:57 -0500

    styling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+// First you will have to store all the sides of the dice as options for the button
+// Then you will make the button function for every time it is clicked
+// The button will have a total of 6 options for every time it is clicked
+// 
+
 // ******************************************************
 // ********************* VARIABLES **********************
 // ******************************************************
+// 
+let rollBtn = document.querySelector (`#rollBtn`)
 
-
+// List all the sides of the dice as variables
 
 
 // ******************************************************
